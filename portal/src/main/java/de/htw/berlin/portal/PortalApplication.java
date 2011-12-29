e30b166bbf9552f582ae7f81dfd3fa3df60da72d
Konrad Fischer <Konrad.Fischer@rittelfischer.com>
2011-12-29 20:50:32 +0100

    Pretty URLs

--- portal/src/main/java/de/htw/berlin/portal/PortalApplication.java
+++ portal/src/main/java/de/htw/berlin/portal/PortalApplication.java
@@ -5,7 +5,9 @@
 package de.htw.berlin.portal;
 
 import de.htw.berlin.portal.auth.RestrictedPageAccessStrategy;
+import de.htw.berlin.portal.home.HomePage;
 import de.htw.berlin.portal.login.LoginPage;
+import de.htw.berlin.portal.registration.RegistrationPage;
 import org.apache.wicket.Page;
 import org.apache.wicket.Session;
 import org.apache.wicket.protocol.http.WebApplication;
@@ -33,15 +35,19 @@ public class PortalApplication extends WebApplication{
 
     @Override
     protected void init() {
-        super.init();
-        
-        
+        super.init();    
         this.getComponentInstantiationListeners().add(new SpringComponentInjector(this));
-        
         RestrictedPageAccessStrategy pageAccessStrategy = new RestrictedPageAccessStrategy();
         this.getSecuritySettings().setAuthorizationStrategy(pageAccessStrategy);
         this.getSecuritySettings().setUnauthorizedComponentInstantiationListener(pageAccessStrategy);
+        mountPages();
         
+    }
+    
+    private void mountPages(){
+        this.mountPage("/registration", RegistrationPage.class);
+        this.mountPage("/login", LoginPage.class);
+        this.mountPage("/home", HomePage.class);
     }
     
     
